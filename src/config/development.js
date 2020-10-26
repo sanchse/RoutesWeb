8a8e210d20879f523322766084e8ab9915d85894
sanchse <sanchse@users.noreply.github.com>
2020-10-26 21:48:14 +0100

    config changes

--- src/config/development.js
+++ src/config/development.js
@@ -1,5 +1,5 @@
 export default {
-    API_URL: 'http://localhost:3000',
+    API_URL: 'http://localhost:3000/api',
     API_USER: 'admin',
     API_SECRET: '12345aA$',
 }
--- src/config/production.js
+++ src/config/production.js
@@ -1,5 +1,5 @@
 export default {
-    API_URL: 'https://localhost:3000',
+    API_URL: 'http://rutas.duckdns.org/api',
     API_USER: 'admin',
     API_SECRET: '12345aA$',
 }
